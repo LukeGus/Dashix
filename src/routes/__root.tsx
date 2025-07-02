0c5e71a2965da161a9935c4f5cc3d0a9d3c6488b
LukeGus <bugattiguy527@gmail.com>
2025-07-02 02:55:37 -0500

    Force dark mode

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,17 @@
 import {Outlet, createRootRoute} from '@tanstack/react-router'
 import {GoogleOAuthProvider} from "@react-oauth/google";
 import {SidebarUI} from "@/components/SidebarUI.tsx";
+import {ThemeProvider} from "@/components/ThemeProvider.tsx";
 
 export const Route = createRootRoute({
     component: () => (
         <GoogleOAuthProvider clientId="871870849443-1402040dfqch4u79jaioo4fjqj3brc6t.apps.googleusercontent.com">
-            <div className="flex min-h-screen">
-                <SidebarUI/>
-            </div>
-            <Outlet/>
+            <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+                <div className="flex min-h-screen">
+                    <SidebarUI/>
+                </div>
+                <Outlet/>
+            </ThemeProvider>
         </GoogleOAuthProvider>
     ),
 })
